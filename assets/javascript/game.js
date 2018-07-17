d90ac314fb5a7a198a58a4d76fdf6c7b663fcc5c
Mason <MasonHester7878@gmail.com>
2018-07-17 03:38:56 -0400

    Full reset, HTML changed, CSS altered, JS reset.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,9 +4,10 @@ $(document).ready(function() {
 
     var timer = 30;
     var userOnQuestion = 0;
-    var correctGuesses = 0;
+    var correctAnswers = 0;
+    var incorrectAnswers = 0;
     var timeOuts = 0;
-    var userAnswer = 0;
+    var totalTime = 0;
 
     //Holding Intervals
 
@@ -15,7 +16,18 @@ $(document).ready(function() {
 
     //Array with all data
 
-    var questions = [
+    var triviaArray = [
+        //Example of format
+        // {
+        //     id: "Question Number",
+        //     question: "Question",
+        //     possibleAnswers: ["Answer A", "Answer B", "Answer C", "Answer D", "Answer E", "..."], (Can be any length)
+        //     correctAnswer: "Answer B",
+        //     tidbit: "Fact about correct answer",
+        //     explanation: "State the correct answer in sentance format",
+        //     image: "Link to image -eg. ./assets/images/nameOfImage.type"
+        // },
+
         {
             id: 1,
             question: "Which fictional city is the home of Batman?",
@@ -119,170 +131,49 @@ $(document).ready(function() {
 
     //====================================================
 
-    //Establishing Functions
+    //Shortcuts
+        //d shorthand for display
+        //r shorthand for row
+        //c shorthand for col
 
-    //Starts game, calls Question Loop
-    
-    function gameStart() {
+    var $dContentCol = $(".displayContentCol");
+
+    var $dStartButton = $(".displayStartButton");
+
+    var $dQuestion = $("<p class='displayQuestion'>");
+
+
+    //====================================================
+
+    //Functions
+
+    function StartGame() {
+        console.log("|Game Start|")
+        resetStats();        
+        displayTrivia();
+    }
+
+    function resetStats() {
         userOnQuestion = 0;
-        correctGuesses = 0;
+        correctAnswers = 0;
+        incorrectAnswers = 0;
         timeOuts = 0;
-        userAnswer = 0;
-        console.log("Game Start");
-        questionLoop();
+        totalTime = 0;
+        console.log("---Stats reset---");
     }
 
-    //Bulk of code inside Question Loop
-    
-    function questionLoop() {
-        //Increments User On Question
-        userOnQuestion++;
-        console.log("Question Loop called| user question: " + userOnQuestion);
-
-        $("#displayTimer").html("<p class='bold'>30</p>");
-
-        //Times out after 30 seconds and calls Time Up
-        timerCountdown = setTimeout(timeUp, 30000);
-
-        //If there is still a question to display it displays it with the answers
-        //Displays a timer counting down
-        if(userOnQuestion < questions.length + 1) {
-            timer = 30;
-            countdown = setInterval(startCountdown, 1000);
-            console.log("Next question called| Question: " + questions[userOnQuestion]);
-            $("#displayQuestion").html("<p class='displayQuestion bold'>" + questions[userOnQuestion - 1].question + "</p>");
-
-            //Displays all the answers            
-            for(i = 0; i < questions[userOnQuestion - 1].possibleAnswers.length; i++) {
-                $("#displayAnswer" + (i + 1)).html("<p id='answer" + (i + 1) + "Button' class='displayAnswerButton displayButton noSelect bold'>" + questions[userOnQuestion - 1].possibleAnswers[i] + "</p>");
-                console.log("Button " + (i + 1) + " created");
-            };
-        }
-
-        else {
-            console.log("gameEnd called");
-            gameEnd();
-        }
-
-        $("#answer1Button").on("click", userInput1);
-        $("#answer2Button").on("click", userInput2);
-        $("#answer3Button").on("click", userInput3);
-        $("#answer4Button").on("click", userInput4);
-        
+    function displayTrivia() {
+        $dContentCol.empty();
+        displayQuestion();
     }
 
-    function userInput1() {
-        console.log("User clicked Button 1");
-        userAnswer = questions[userOnQuestion - 1].possibleAnswers[0]
-        answerCheck()
-    }
-    
-    function userInput2() {
-        console.log("User clicked Button 2");
-        userAnswer = questions[userOnQuestion - 1].possibleAnswers[1]
-        answerCheck()
+    function displayQuestion () {
+        $dContentCol.append($dQuestion);
     }
 
-    function userInput3() {
-        console.log("User clicked Button 3");
-        userAnswer = questions[userOnQuestion - 1].possibleAnswers[2]
-        answerCheck()
-    }
 
-    function userInput4() {
-        console.log("User clicked Button 4");
-        userAnswer = questions[userOnQuestion - 1].possibleAnswers[3]
-        answerCheck()
-    }
 
-    function answerCheck() {
-        console.log("checking answer");
-        clearTimeout(timerCountdown);
-        clearInterval(countdown);
+    //====================================================
 
-        if(userAnswer === questions[userOnQuestion - 1].correctAnswer) {
-            correctAnswer();
-        }
-
-        else {
-            incorrectAnswer();
-        }
-    }
-
-    function startCountdown() {
-        timer--;
-        console.log("second passed");
-        $("#displayTimer").html("<p class='noSelect bold'>" + timer + "</p>");
-    }
-
-    function correctAnswer() {
-        console.log("Correct answer")
-        $("#displayQuestion").html("<p class='bold'>Correct!</p>");
-        $("#displayAnswer1").html("<p class='bold'>" + questions[userOnQuestion - 1].tidbit + "</p>");
-        $("#displayAnswer2").html("<img class='displayImage' src='" + questions[userOnQuestion - 1].image + "'>");
-        $("#displayAnswer3").html("<p></p>");
-        $("#displayAnswer4").html("<p></p>");
-        correctGuesses++;
-        setTimeout(questionLoop, 2000);
-    }
-
-    function incorrectAnswer() {
-        console.log("Incorrect answer")
-        $("#displayQuestion").html("<p class='bold'>Incorrect.</p>");
-        $("#displayAnswer1").html("<p class='bold'>" + questions[userOnQuestion - 1].explanation + "</p>");
-        $("#displayAnswer2").html("<img class='displayImage' src='" + questions[userOnQuestion - 1].image + "'>");
-        $("#displayAnswer3").html("<p></p>");
-        $("#displayAnswer4").html("<p></p>");
-        setTimeout(questionLoop, 2000);
-    }
-
-    // function marcTimer(numberSeconds, nextPage) {
-    //     setTimeout(doAThing, 
-    //         amount)
-    // }
-
-    // when on a page requiring user input: marcTimer(30, answer.html)
-    // when on a page showing answer: marcTimer(3, nextquestion.html)
-
-    function timeUp() {
-        $("#displayTimer").html("<p class='bold'>0</p>")
-        clearInterval(countdown);
-        timeOuts++;
-
-        if(userOnQuestion < 11) {
-            $("#displayQuestion").html("<p class='bold'>Times up!</p>");
-            $("#displayAnswer1").html("<p class='bold'>" + questions[userOnQuestion - 1].explanation + "</p>");
-            $("#displayAnswer2").html("<img class='displayImage' src='" + questions[userOnQuestion - 1].image + "'>");
-            $("#displayAnswer3").html("<p></p>");
-            $("#displayAnswer4").html("<p></p>");
-            setTimeout(questionLoop, 2000);
-        }
-        
-        else {
-            gameEnd();
-        }
-    }
-        
-    function gameEnd() {
-        clearTimeout(timerCountdown);
-        clearInterval(countdown);
-
-        $("#displayQuestion").html("<p class='bold'>Game over.</p>");
-        $("#displayAnswer1").html("<p class='bold'>You answered " + correctGuesses + " questions correctly!</p>");
-        $("#displayAnswer2").html("<p class='bold'>You timed out on " + timeOuts + " questions.</p>");
-
-        if(correctGuesses > 8) {
-            $("#displayAnswer3").html("<img class='displayImage finalImage' src='./assets/images/goodScore.jpg'>");
-        }
-
-        else {
-            $("#displayAnswer3").html("<img class='displayImage finalImage' src='./assets/images/badScore.jpg'>");
-        }
-
-        $("#displayAnswer4").html("<p class='displayStartButton displayButton bold noSelect'>Try again?</p>");
-    }
-
-    //==============================================================
-
-    $("body").on("click", ".displayStartButton", gameStart);
+    $dContentCol.on("click", $dStartButton, StartGame);
 });
